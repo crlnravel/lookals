cfabcd2fd288a4765bbeecf8aa64204f76c7b8f2
Carleano Ravelza Wongso <carleanoravel@gmail.com>
2026-06-30 18:46:00 +0700

    Initial Commit

---
+++ Lookals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lookals
+//
+//  Created by Carleano Ravelza Wongso on 30/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lookals/LookalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookalsApp.swift
+//  Lookals
+//
+//  Created by Carleano Ravelza Wongso on 30/06/26.
+//
+
+import SwiftUI
+
+@main
+struct LookalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
